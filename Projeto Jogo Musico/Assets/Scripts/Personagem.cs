84d1c99d8d004987d0906cbf191d093976eb25f6
Bruno Nascimento da Rocha Pita <bruno__nascimento@outlook.com>
2021-03-31 13:57:50 -0300

    update script player
    Adicionei collider que destroi caso o personagem toque num objeto com a tag obstaculo e ele anda pra direita

--- Projeto Jogo Musico/Assets/Scripts/Personagem.cs
+++ Projeto Jogo Musico/Assets/Scripts/Personagem.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Personagem : MonoBehaviour
 {
     // Start is called before the first frame update
+     private float velocidade=1;
     void Start()
     {
         
@@ -13,6 +14,15 @@ public class Personagem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+         transform.Translate(Vector2.right * velocidade * Time.deltaTime); 
+    }
+    
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+     if (other.gameObject.tag == "obstaculo")
+    {
+   Destroy(gameObject);
+    }
+    
     }
 }
