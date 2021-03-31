faf44bb072c2b9c2dedafe9f34be98c0f2498a4e
Bruno Nascimento da Rocha Pita <bruno__nascimento@outlook.com>
2021-03-31 14:50:54 -0300

    upd adjust locations

--- Projeto Jogo Musico/Assets/Scripts/Personagem.cs
+++ Projeto Jogo Musico/Assets/Scripts/Personagem.cs
@@ -6,6 +6,7 @@ public class Personagem : MonoBehaviour
 {
     // Start is called before the first frame update
      private float velocidade=1;
+     Vector3 posicao= new Vector3 (-16,4,0);
     void Start()
     {
         
@@ -21,7 +22,7 @@ public class Personagem : MonoBehaviour
     {
      if (other.gameObject.tag == "obstaculo")
     {
-   Destroy(gameObject);
+  Destroy(gameObject);
     }
     
     }
