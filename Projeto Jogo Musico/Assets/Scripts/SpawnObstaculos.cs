a0bb2eb455c47cda67bad5f3ca7ceb6998b9abac
AndreZrem <andrecancerigina@gmail.com>
2021-04-02 15:30:24 -0300

    Adc spawn

--- Projeto Jogo Musico/Assets/Scripts/SpawnObstaculos.cs
+++ Projeto Jogo Musico/Assets/Scripts/SpawnObstaculos.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class SpawnObstaculos : MonoBehaviour
 {
+
+    public GameObject obst;
+    public float limites;
+    public float tempoMaximo = 3f;
+
+    private float tempo = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject newObst = Instantiate(obst);
+        newObst.transform.position = transform.position + new Vector3(0,Random.Range(-limites, limites), 0 );
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (tempo> tempoMaximo)
+        {
+            GameObject newObst = Instantiate(obst);
+            newObst.transform.position = transform.position + new Vector3(0,Random.Range(-limites, limites), 0 );
+            Destroy(newObst, 15f);
+            tempo = 0;
+        }
+
+        tempo += Time.deltaTime;
     }
 }
