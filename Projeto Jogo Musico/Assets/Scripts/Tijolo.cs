f879838f66b509ccfbb9e10d7dd042ed5162ac7d
André Menezes <andrecancerigina@gmail.com>
2021-04-22 20:31:32 -0300

    Tijolo
    Fiz o codigo de ficar subindo e descendo

---
+++ Projeto Jogo Musico/Assets/Scripts/Tijolo.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tijolo : MonoBehaviour
+{
+
+    public float tempoMinimo = 3f;
+    public float tempoMaximo;
+    public float speed;
+    private float tempo = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempo += Time.deltaTime;
+
+        if (tempo< tempoMinimo)
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+
+        if(tempo> tempoMinimo)
+        {
+        transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+
+        if(tempo > tempoMaximo)
+        {
+            Debug.Log("funcionou");
+            tempo = 0;
+        }
+
+    }
+}
