f866aaec16020da42b083c668870e56afd913f6e
André Menezes <andrecancerigina@gmail.com>
2021-04-30 20:18:39 -0300

    Fase da chuva e select fase
    Criei a chave com uma chuvinha meio vagabunda vou tentar melhorar depois caso não consiga irei tirar, e fiz o seletor de fase basico que tambem vou tentar melhorar

--- Projeto Jogo Musico/Assets/Scripts/SceneSwitchScript.cs
+++ Projeto Jogo Musico/Assets/Scripts/SceneSwitchScript.cs
@@ -6,9 +6,19 @@ using UnityEngine.SceneManagement;
 public class SceneSwitchScript : MonoBehaviour
 {
 
-    public void EscolherFase()
+    public void EscolherFase1()
     {
         SceneManager.LoadScene("Fase1");
+    }
+
+    public void EscolherFase2()
+    {
+        SceneManager.LoadScene("Fase2");
+    }
+
+    public void EscolherFase3()
+    {
+        SceneManager.LoadScene("Fase3");
     }
 
     public void Sair()
