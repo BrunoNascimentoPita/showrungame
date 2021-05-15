af4ce0858e89d40f68b13af670bde07dcef1baa9
André Menezes <andrecancerigina@gmail.com>
2021-05-15 15:33:34 -0300

    a

--- Projeto Jogo Musico/Assets/Scripts/AudioController.cs
+++ Projeto Jogo Musico/Assets/Scripts/AudioController.cs
@@ -1,11 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AudioController : MonoBehaviour
 {
+    private static AudioController instance;
 
-    public AudioSource audioSourceMusicaDeFundo;
+    public AudioSource BGM;
+    public AudioClip track1;
+    public AudioClip track2; 
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +37,16 @@ public class AudioController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(SceneManager.GetActiveScene().buildIndex == 2)
+        {
+            
+        }
+
+        if()
+        {
+
+        }
+
+
     }
 }
