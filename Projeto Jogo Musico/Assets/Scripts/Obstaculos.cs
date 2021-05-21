9346e25c48b2de4fab8175ba1c8b0410232a15e4
André Menezes <andrecancerigina@gmail.com>
2021-05-21 18:29:56 -0300

    Obstaculos rotacionando

--- Projeto Jogo Musico/Assets/Scripts/Obstaculos.cs
+++ Projeto Jogo Musico/Assets/Scripts/Obstaculos.cs
@@ -6,10 +6,19 @@ public class Obstaculos : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed;
+    public float zRotation;
 
     // Update is called once per frame
     void Update()
     {
         transform.position += Vector3.down * speed * Time.deltaTime;
+        rotationObst();
+        
+    }
+
+    private void rotationObst()
+    {
+        zRotation = zRotation + Time.deltaTime * 50;
+        transform.rotation = Quaternion.Euler(0, 0, zRotation);
     }
 }
